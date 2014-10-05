5573bb04b5f994beabe025fa4d661930b6d98f02
Luca Sbardella <luca.sbardella@gmail.com>
2014-10-05 16:49:20 +0100

    removed master

--- tests.js
+++ tests.js
@@ -1,8 +1,11 @@
     //
-    describe("Test extension object", function() {
+    describe("Test require object", function() {
 
         it("Check basic properties", function() {
             expect(typeof(require)).toBe('object');
             expect(typeof(require.paths)).toBe('object');
+            expect(typeof(require.shim)).toBe('object');
+            expect(require.deps instanceof Array).toBe(true);
+            expect(typeof(require.buildUrl)).toBe('string');
         });
     });
